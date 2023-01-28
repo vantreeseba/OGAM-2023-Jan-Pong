b5237bdbdd38800e292593d0c6a4c9c629d50eda
Ben Van Treese <vantreeseba@gmail.com>
2023-01-28 16:12:57 -0600

    feat: Add more docs to code.

--- Assets/Scripts/Gameplay/Paddle.cs
+++ Assets/Scripts/Gameplay/Paddle.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Paddle : MonoBehaviour {
@@ -8,26 +9,36 @@ public class Paddle : MonoBehaviour {
   private PlayerControls _controls;
   private float _movement;
 
-  void Start() {
+  void Awake() {
     _controls = new PlayerControls();
     _controls.Enable();
     _controls.Gameplay.Enable();
-
-    _controls.Gameplay.Move.performed += ctx => _movement = ctx.ReadValue<float>();
-
   }
 
+  void OnEnable() => _controls.Gameplay.Move.performed += OnMove;
+  void OnDisable() => _controls.Gameplay.Move.performed -= OnMove;
+  void OnMove(InputAction.CallbackContext ctx) => _movement = ctx.ReadValue<float>();
+
   void Update() {
-    transform.position += movementSpeed * Time.deltaTime * new Vector3(0, _movement, 0);
+    // Move the paddle based on the current input value.
+    transform.position += movementSpeed * Time.deltaTime * Vector3.zero.SetY(_movement);
+    var position = transform.position;
 
-    var pos = transform.position;
-    if (pos.y > 20) {
-      transform.position = new Vector3(pos.x, 20, pos.z);
-    }
-    if (pos.y < -20) {
-      transform.position = new Vector3(pos.x, -20, pos.z);
-    }
+    // calculate the size of the paddle.
+    // Divide in 2 so we can use it to prevent paddle from leaving screen.
+    var size = transform.localScale.y / 2;
 
-    // transform.localScale = new Vector3(1, width, 1);
+    // If the position of "origin" or center of the paddle, plus it's height is greater than the
+    // size of the screen, we have hit the top or bottom.
+    var hitTopOfScreen = position.y + size > GameState.screenSize.y;
+    var hitBottomOfScreen = position.y - size < -GameState.screenSize.y;
+
+    // If we hit either the top or the bottom, keep the y portion of the position from going past the screen.
+    if (hitTopOfScreen) {
+      transform.position = position.SetY(GameState.screenSize.y - size);
+    }
+    if (hitBottomOfScreen) {
+      transform.position = position.SetY(-GameState.screenSize.y + size);
+    }
   }
 }
